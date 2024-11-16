1b666bc8de123c6e191d14a745eb140a3f59466b
huamin <huamin.li@yo-star.com>
2024-11-16 16:32:54 +0800

    猜数比较大小

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("猜数游戏!");
     let mut thread_rng = rand::thread_rng();
     let secret_number = thread_rng.gen_range(1..=101);
     println!("神秘数字是：{}", secret_number);
-    println!("猜数： ");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-    .expect("Failed to read line");
-    
-    println!("你猜的数字是：{}", guess);
+    loop {
+        println!("请输入一个数字 ");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // 转化数据类型
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("输入非法，请输入一个数字");
+                continue;
+            }
+        };
+
+        println!("你猜的数字是：{}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了"),
+            Ordering::Greater => println!("太大了"),
+            Ordering::Equal => {
+                println!("猜对了");
+                break;
+            }
+        }
+    }
 }
