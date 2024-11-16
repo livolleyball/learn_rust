5bdaeec02d0bd3aa6b1d433a17d71530693cc0c6
huamin <huamin.li@yo-star.com>
2024-11-16 16:19:15 +0800

    猜数游戏

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # install 
 参考 https://www.rust-lang.org/zh-CN/learn/get-started
 Windows 要下载 rustup-init.exe 可执行文件。
+
+
+# library crate
+[crates.io](https://crates.io/) 是Rust编程语言的中央包注册表,提供了共享“crates”的平台,这些crates是Rust编写的库,可用于增强您的项目
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    // Your program will start here.
-    println!("Hello world!");
+    println!("猜数游戏!");
+    let mut thread_rng = rand::thread_rng();
+    let secret_number = thread_rng.gen_range(1..=101);
+    println!("神秘数字是：{}", secret_number);
+    println!("猜数： ");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+    .expect("Failed to read line");
+    
+    println!("你猜的数字是：{}", guess);
 }
