4e406e104710ea8376f74d3ea2686c89efe7435e
elliot_li <huamin.li@yo-star.com>
2024-11-18 13:36:56 +0800

    函数 loop  和 control flow

--- readme.md
+++ readme.md
@@ -5,3 +5,8 @@ Windows 要下载 rustup-init.exe 可执行文件。
 
 # library crate
 [crates.io](https://crates.io/) 是Rust编程语言的中央包注册表,提供了共享“crates”的平台,这些crates是Rust编写的库,可用于增强您的项目
+
+
+# 命名规范
+* 常量全部使用大写字母，每一个单词之间用下划线分开；
+* 函数名使用 snake case 命名法，第一个单词小写，之后每个单词首字母小写
--- src/demo.rs
+++ src/demo.rs
@@ -13,13 +13,18 @@ fn main() {
     let x = 2.0;
     let y: f32 = 2.0;
     println!(" x is  {} , y is {} ", x, y);
-    
-    
 
     fn_numeric_perations();
     fn_bool();
     fn_char();
     fn_tuple();
+    another_function(5);
+    control_flow_1();
+    control_flow_2();
+    fn_loop_1();
+    fn_while_1();
+    fn_for_1();
+    fn_for_2();
 
     let months = [
         "January",
@@ -72,7 +77,6 @@ fn fn_numeric_perations() {
     println!("remainder is  {} ", remainder);
 }
 
-
 fn fn_tuple() {
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     let (x, y, z) = tup;
@@ -82,4 +86,65 @@ fn fn_tuple() {
     // 点标记法  a period (.) followed by the index of the value we want to access
     let five_hundred = x.0;
     println!("five_hundred is {}", five_hundred)
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn control_flow_1() {
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}
+
+fn control_flow_2() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+}
+
+// loop while for
+
+fn fn_loop_1() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The fn_loop  result is {result}");
+}
+
+fn fn_while_1() {
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+    println!("等于 {}!!!", number);
+}
+
+fn fn_for_1() {
+    let x: [i32; 6] = [1, 2, 3, 4, 5, 6];
+    for element in x.iter() {
+        println!("the value is: {}", element);
+    }
+}
+fn fn_for_2() {
+    for number in (1..4).rev(){
+        println!("{number}!!!")
+    }
+    println!("fn_for_2")
+    
 }
